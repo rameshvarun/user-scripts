8c397ee3a3843c9d438227c91e56fb43b6f2415c
Varun Ramesh <rameshvarun@users.noreply.github.com>
2017-12-16 03:00:57 -0800

    Don't conflict with the page's jQuery.

--- hacker-news-read-later-pinboard.user.js
+++ hacker-news-read-later-pinboard.user.js
@@ -12,12 +12,24 @@
 (function() {
     'use strict';
 
-    window.pinboard_links = {};
-    window.pinboard_titles = {};
-    $('tr.athing').each(function(i, tr) {
-        var a = $(tr).find('a.storylink');
-        window.pinboard_links[i] = a.attr('href');
-        window.pinboard_titles[i] = a.html();
-        $(tr).next().children('td.subtext').append(` | <a href="javascript:q=window.pinboard_links[${i}];p=window.pinboard_titles[${i}];void(t=open('https://pinboard.in/add?later=yes&noui=yes&jump=close&url='+encodeURIComponent(q)+'&title='+encodeURIComponent(p),'Pinboard','toolbar=no,width=100,height=100'));t.blur();">read later [pinboard]</a>`);
+    // Don't conflict with page's jQuery.
+    $.noConflict();
+    var jq = jQuery;
+
+    // Dict of functions that save to pinboard.
+    window.save_to_pinboard = {};
+
+    jq('tr.athing:not(.comtr)').each(function(i, tr) {
+        var a = jq(tr).find('a.storylink');
+        var link = a.attr('href');
+        var title = a.html();
+
+        window.save_to_pinboard[i] = function() {
+            var url = `https://pinboard.in/add?later=yes&noui=yes&jump=close&url=${encodeURIComponent(link)}&title=${encodeURIComponent(title)}`;
+            var popup = window.open(url,'Pinboard','toolbar=no,width=100,height=100');
+            popup.blur();
+        };
+
+        jq(tr).next().children('td.subtext').append(` | <a href="javascript:window.save_to_pinboard[${i}]()">read later [pinboard]</a>`);
     });
 })();
