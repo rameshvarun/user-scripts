f248f9101632acbbe88ef33f028ba272e08bb7ab
Varun Ramesh <rameshvarun@users.noreply.github.com>
2017-12-18 08:46:54 -0700

    Support youtu.be, added playlist embed.

--- pinboard-youtube-embed.user.js
+++ pinboard-youtube-embed.user.js
@@ -18,14 +18,27 @@
 
     GM_addStyle (GM_getResourceText ("LAZY_YT_CSS"));
 
+    function video_embed(yt_id) {
+       return `<div style="margin-top:8px; margin-bottom:3px;" class="lazyYT" data-width="560" data-height="315" data-youtube-id="${yt_id}" data-ratio="16:9"></div>`;
+    }
+
     $('div.bookmark').each(function(i, b) {
         var url = URI($(b).find('a.bookmark_title').attr('href'));
-        if (url.origin() == "https://www.youtube.com") {
+        if (url.origin() == "https://www.youtube.com" || url.origin() == "http://www.youtube.com") {
             if(url.path() == "/watch") {
-                var query = URI.parseQuery(url.query());
+                let query = URI.parseQuery(url.query());
                 var yt_id = query.v;
-                $(b).find('div.display').append(`<div style="margin-top:8px; margin-bottom:3px;" class="lazyYT" data-width="560" data-height="315" data-youtube-id="${yt_id}" data-ratio="16:9"></div>`);
+                $(b).find('div.display').append(video_embed(yt_id));
             }
+            if(url.path() == "/playlist") {
+                let query = URI.parseQuery(url.query());
+                var playlist_id = query.list;
+                $(b).find('div.display').append(`<iframe style="margin-top:8px; margin-bottom:3px;" width="560" height="315" src="https://www.youtube.com/embed/videoseries?list=${playlist_id}" frameborder="0" allowfullscreen></iframe>`);
+            }
+        }
+        if (url.origin() == "https://youtu.be" || url.origin() == "http://youtu.be") {
+            let yt_id = url.path().substring(1);
+            $(b).find('div.display').append(video_embed(yt_id));
         }
     });
 
